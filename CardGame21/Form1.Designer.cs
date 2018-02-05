57940f6476064a616833c3e89bcac68dc8fe3362
Derek B. Sneddon <Jexout48@gmail.com>
2018-02-05 14:28:31 -0700

    Design Push

--- CardGame21/Form1.Designer.cs
+++ CardGame21/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CardGame21
 {
-    partial class Form1
+    partial class frm21
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCard1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player\'s Cards";
+            // 
+            // lblCard1
+            // 
+            this.lblCard1.AutoSize = true;
+            this.lblCard1.Location = new System.Drawing.Point(14, 50);
+            this.lblCard1.Name = "lblCard1";
+            this.lblCard1.Size = new System.Drawing.Size(13, 13);
+            this.lblCard1.TabIndex = 1;
+            this.lblCard1.Text = "9";
+            // 
+            // frm21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(658, 458);
+            this.Controls.Add(this.lblCard1);
+            this.Controls.Add(this.label1);
+            this.Name = "frm21";
+            this.Text = "Card Game 21 Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCard1;
     }
 }
 
---
+++ CardGame21/frm21.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace CardGame21
+{
+    partial class frm21
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCard1 = new System.Windows.Forms.Label();
+            this.lblCard2 = new System.Windows.Forms.Label();
+            this.lblCard3 = new System.Windows.Forms.Label();
+            this.lblCard4 = new System.Windows.Forms.Label();
+            this.lblDealer1 = new System.Windows.Forms.Label();
+            this.lblDealer2 = new System.Windows.Forms.Label();
+            this.lblDealer3 = new System.Windows.Forms.Label();
+            this.lblDealer4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnDeal = new System.Windows.Forms.Button();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnScore = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblPlayerScore = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblDealerScore = new System.Windows.Forms.Label();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player\'s Cards";
+            // 
+            // lblCard1
+            // 
+            this.lblCard1.AutoSize = true;
+            this.lblCard1.BackColor = System.Drawing.Color.White;
+            this.lblCard1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCard1.ForeColor = System.Drawing.Color.Red;
+            this.lblCard1.Location = new System.Drawing.Point(14, 50);
+            this.lblCard1.Name = "lblCard1";
+            this.lblCard1.Size = new System.Drawing.Size(100, 110);
+            this.lblCard1.TabIndex = 1;
+            this.lblCard1.Text = "9";
+            this.lblCard1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCard1.Visible = false;
+            // 
+            // lblCard2
+            // 
+            this.lblCard2.AutoSize = true;
+            this.lblCard2.BackColor = System.Drawing.Color.White;
+            this.lblCard2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCard2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCard2.ForeColor = System.Drawing.Color.Red;
+            this.lblCard2.Location = new System.Drawing.Point(143, 50);
+            this.lblCard2.Name = "lblCard2";
+            this.lblCard2.Size = new System.Drawing.Size(100, 110);
+            this.lblCard2.TabIndex = 2;
+            this.lblCard2.Text = "9";
+            this.lblCard2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCard2.Visible = false;
+            // 
+            // lblCard3
+            // 
+            this.lblCard3.AutoSize = true;
+            this.lblCard3.BackColor = System.Drawing.Color.White;
+            this.lblCard3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCard3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCard3.ForeColor = System.Drawing.Color.Red;
+            this.lblCard3.Location = new System.Drawing.Point(279, 50);
+            this.lblCard3.Name = "lblCard3";
+            this.lblCard3.Size = new System.Drawing.Size(100, 110);
+            this.lblCard3.TabIndex = 3;
+            this.lblCard3.Text = "9";
+            this.lblCard3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCard3.Visible = false;
+            // 
+            // lblCard4
+            // 
+            this.lblCard4.AutoSize = true;
+            this.lblCard4.BackColor = System.Drawing.Color.White;
+            this.lblCard4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCard4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCard4.ForeColor = System.Drawing.Color.Red;
+            this.lblCard4.Location = new System.Drawing.Point(412, 50);
+            this.lblCard4.Name = "lblCard4";
+            this.lblCard4.Size = new System.Drawing.Size(100, 110);
+            this.lblCard4.TabIndex = 4;
+            this.lblCard4.Text = "9";
+            this.lblCard4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCard4.Visible = false;
+            // 
+            // lblDealer1
+            // 
+            this.lblDealer1.AutoSize = true;
+            this.lblDealer1.BackColor = System.Drawing.Color.White;
+            this.lblDealer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDealer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealer1.ForeColor = System.Drawing.Color.Red;
+            this.lblDealer1.Location = new System.Drawing.Point(14, 216);
+            this.lblDealer1.Name = "lblDealer1";
+            this.lblDealer1.Size = new System.Drawing.Size(100, 110);
+            this.lblDealer1.TabIndex = 5;
+            this.lblDealer1.Text = "9";
+            this.lblDealer1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDealer1.Visible = false;
+            // 
+            // lblDealer2
+            // 
+            this.lblDealer2.AutoSize = true;
+            this.lblDealer2.BackColor = System.Drawing.Color.White;
+            this.lblDealer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDealer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealer2.ForeColor = System.Drawing.Color.Red;
+            this.lblDealer2.Location = new System.Drawing.Point(143, 216);
+            this.lblDealer2.Name = "lblDealer2";
+            this.lblDealer2.Size = new System.Drawing.Size(100, 110);
+            this.lblDealer2.TabIndex = 6;
+            this.lblDealer2.Text = "9";
+            this.lblDealer2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDealer2.Visible = false;
+            // 
+            // lblDealer3
+            // 
+            this.lblDealer3.AutoSize = true;
+            this.lblDealer3.BackColor = System.Drawing.Color.White;
+            this.lblDealer3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDealer3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealer3.ForeColor = System.Drawing.Color.Red;
+            this.lblDealer3.Location = new System.Drawing.Point(279, 216);
+            this.lblDealer3.Name = "lblDealer3";
+            this.lblDealer3.Size = new System.Drawing.Size(100, 110);
+            this.lblDealer3.TabIndex = 7;
+            this.lblDealer3.Text = "9";
+            this.lblDealer3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDealer3.Visible = false;
+            // 
+            // lblDealer4
+            // 
+            this.lblDealer4.AutoSize = true;
+            this.lblDealer4.BackColor = System.Drawing.Color.White;
+            this.lblDealer4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDealer4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealer4.ForeColor = System.Drawing.Color.Red;
+            this.lblDealer4.Location = new System.Drawing.Point(412, 216);
+            this.lblDealer4.Name = "lblDealer4";
+            this.lblDealer4.Size = new System.Drawing.Size(100, 110);
+            this.lblDealer4.TabIndex = 8;
+            this.lblDealer4.Text = "9";
+            this.lblDealer4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDealer4.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(10, 175);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(129, 24);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Player\'s Cards";
+            // 
+            // btnDeal
+            // 
+            this.btnDeal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeal.Location = new System.Drawing.Point(613, 50);
+            this.btnDeal.Name = "btnDeal";
+            this.btnDeal.Size = new System.Drawing.Size(75, 39);
+            this.btnDeal.TabIndex = 10;
+            this.btnDeal.Text = "&Deal";
+            this.btnDeal.UseVisualStyleBackColor = true;
+            // 
+            // btnHit
+            // 
+            this.btnHit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHit.Location = new System.Drawing.Point(613, 113);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(75, 39);
+            this.btnHit.TabIndex = 11;
+            this.btnHit.Text = "&Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            // 
+            // btnScore
+            // 
+            this.btnScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScore.Location = new System.Drawing.Point(613, 175);
+            this.btnScore.Name = "btnScore";
+            this.btnScore.Size = new System.Drawing.Size(75, 39);
+            this.btnScore.TabIndex = 12;
+            this.btnScore.Text = "&Score";
+            this.btnScore.UseVisualStyleBackColor = true;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbout.Location = new System.Drawing.Point(613, 239);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 39);
+            this.btnAbout.TabIndex = 13;
+            this.btnAbout.Text = "&About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(613, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "E&xit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(275, 13);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 24);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Score";
+            // 
+            // lblPlayerScore
+            // 
+            this.lblPlayerScore.AutoSize = true;
+            this.lblPlayerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerScore.Location = new System.Drawing.Point(341, 13);
+            this.lblPlayerScore.Name = "lblPlayerScore";
+            this.lblPlayerScore.Size = new System.Drawing.Size(20, 24);
+            this.lblPlayerScore.TabIndex = 16;
+            this.lblPlayerScore.Text = "0";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(275, 175);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(60, 24);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "Score";
+            // 
+            // lblDealerScore
+            // 
+            this.lblDealerScore.AutoSize = true;
+            this.lblDealerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerScore.Location = new System.Drawing.Point(341, 175);
+            this.lblDealerScore.Name = "lblDealerScore";
+            this.lblDealerScore.Size = new System.Drawing.Size(20, 24);
+            this.lblDealerScore.TabIndex = 18;
+            this.lblDealerScore.Text = "0";
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.Location = new System.Drawing.Point(13, 366);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(675, 24);
+            this.lblWinner.TabIndex = 19;
+            this.lblWinner.Text = "Play to see if you can win!";
+            this.lblWinner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frm21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 513);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.lblDealerScore);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lblPlayerScore);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnScore);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.btnDeal);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblDealer4);
+            this.Controls.Add(this.lblDealer3);
+            this.Controls.Add(this.lblDealer2);
+            this.Controls.Add(this.lblDealer1);
+            this.Controls.Add(this.lblCard4);
+            this.Controls.Add(this.lblCard3);
+            this.Controls.Add(this.lblCard2);
+            this.Controls.Add(this.lblCard1);
+            this.Controls.Add(this.label1);
+            this.Name = "frm21";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCard1;
+        private System.Windows.Forms.Label lblCard2;
+        private System.Windows.Forms.Label lblCard3;
+        private System.Windows.Forms.Label lblCard4;
+        private System.Windows.Forms.Label lblDealer1;
+        private System.Windows.Forms.Label lblDealer2;
+        private System.Windows.Forms.Label lblDealer3;
+        private System.Windows.Forms.Label lblDealer4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnDeal;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnScore;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblPlayerScore;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblDealerScore;
+        private System.Windows.Forms.Label lblWinner;
+    }
+}
+
